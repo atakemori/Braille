337069d21bd8e2a768582eeec5cb2713240c04f2
atakemori <atakemori99@gmail.com>
2020-11-09 11:48:35 -0800

    Add Privacy Policy and dialog
    Added a privacy policy to the git repo and as an xml string resource within the app.  While the other preferences currently link to github links for the license and feedback/issues, the privacy policy is popped up in-app as an alertDialog.  Later might want to differentiate between preference onTouch for alertDialogs, and onLongTouch for launching intents out-of-app.
    
    Also removed Dark theme entry for now, so only Light is available.
    
    Signed-off-by: atakemori <atakemori99@gmail.com>

--- app/src/main/java/com/takemori/braille/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/takemori/braille/ui/settings/SettingsFragment.java
@@ -1,5 +1,6 @@
 package com.takemori.braille.ui.settings;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,9 +9,11 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceScreen;
 
@@ -31,6 +34,33 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.preference_screen, rootKey);
+
+        // Find the preference about the privacy statement and launch a dialog onClick.
+        Preference privacyPreference = findPreference("privacy");
+        privacyPreference.setOnPreferenceClickListener(preference -> {
+            privacyLaunchDialog();
+            return true;
+        });
     }
-    
+
+
+    // Generate the onClick functions of options settings after the preference fragment is inflated
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    public AlertDialog privacyNoticeDialogBuilder() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(R.string.privacy_policy_title);
+        builder.setMessage(R.string.privacy_policy_V1_0);
+        return builder.create();
+    }
+
+    public void privacyLaunchDialog() {
+        AlertDialog privacyAlert = privacyNoticeDialogBuilder();
+        privacyAlert.show();
+    }
+
+
 }
